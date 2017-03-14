d63cd5909e57405c8c7b20dd95e0c945cb9272fb
trevormac <trevor.c.macgregor@gmail.com>
2017-03-14 18:37:00 -0400

    Initial Commit

---
+++ AutoLayout-Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout-Storyboard
+//
+//  Created by Trevor MacGregor on 2017-03-14.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout-Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout-Storyboard
+//
+//  Created by Trevor MacGregor on 2017-03-14.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
